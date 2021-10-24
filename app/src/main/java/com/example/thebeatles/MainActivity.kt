bbcc0b186d2f584fcca5c754881a8a479bbc8529
tulilyngo <tulilyngo@gmail.com>
2021-10-24 00:01:58 -0500

    Add bottom navigation bar

--- app/src/main/java/com/example/thebeatles/MainActivity.kt
+++ app/src/main/java/com/example/thebeatles/MainActivity.kt
@@ -2,10 +2,27 @@ package com.example.thebeatles
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+  companion object {
+    private var instance: MainActivity? = null
+
+    public fun getInstance(): MainActivity {
+      return instance!!
+    }
+  }
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    instance = this // have to move here
     setContentView(R.layout.activity_main)
+
+    var bottomNavigation = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+    val navController = Navigation.findNavController(this, R.id.fragment)
+    bottomNavigation.setupWithNavController(navController)
   }
 }
